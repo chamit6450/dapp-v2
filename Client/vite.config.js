6724ac5f8a49f38c1d837269897943b7be89a84d
chamit6450 <chamit6450@gmail.com>
2025-01-06 13:16:35 +0530

    pkg upd

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; // Correct React import
 import { nodePolyfills } from 'vite-plugin-node-polyfills'; // Correct polyfills import
-import vercel from 'vite-plugin-vercel';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     nodePolyfills(), // Ensure node polyfills are added first
     react(), // React plugin for Vite
-    vercel()
   ],
  
 });
